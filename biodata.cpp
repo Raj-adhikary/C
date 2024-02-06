9889b16e3e90cd8da0dd8db3c464e0dcfbcec8b2
Raj-adhikary <adhikaryraj411@gmail.com>
2024-02-06 10:50:58 +0600

    class

--- biodata.cpp
+++ biodata.cpp
@@ -1,1 +1,22 @@
 
+#include<iostream>
+using namespace std;
+class student {
+    public:
+int id;
+double gpa;
+void display(){
+  cout<<"id = "<<id<<" and "<<gpa<<endl;
+}
+};
+
+int main(){
+student prince,rana;
+prince.id=10;
+prince.gpa=3.45;
+prince.display();
+rana.id=12;
+rana.gpa=3.34;
+rana.display();
+
+}
