e52ad869a45f0bb0f33216cf42adae7366d1cdba
Raj-adhikary <adhikaryraj411@gmail.com>
2024-02-07 07:35:44 +0600

    merge short

--- biodata.cpp
+++ biodata.cpp
@@ -3,20 +3,58 @@
 using namespace std;
 class student {
     public:
-int id;
+string name,session,exam1,pren1,ct;
+int id,a;
+
 double gpa;
 void display(){
-  cout<<"id = "<<id<<" and "<<gpa<<endl;
+  cout<<"name = "<<name<<endl<<"session = "<<session<<endl;
+  cout<<"id = "<<id<<" and "<<" cgpa id = "<<gpa<<endl;
+  cout<<endl;
+
 }
+  student(string name1,string session1,int id1,double b){
+  id=id1;
+  gpa=b;
+  name=name1; 
+  session=session1;
+  }
+  void display2(int a1){
+
+    a=a1;
+    cout<<"CT result = "<<a<<endl;
+  }
+
 };
 
 int main(){
-student prince,rana;
-prince.id=10;
-prince.gpa=3.45;
-prince.display();
-rana.id=12;
-rana.gpa=3.34;
-rana.display();
-cout<<"hi";
+student s1("Raj Adhikary","2019-2020",190109,3.45);
+student s2("Joydip das","2019-2020",190102,3.57);
+student s3("SHOPON ahmed","2019-2020",190107,3.75);
+student s4("Tasmia","2019-2020",190154,3.42);
+student s5("Rahul roy","2019-2020",190108,3.41);
+
+
+s1.display();
+
+s1.display2(10);
+
+s2.display();
+
+s1.display2(01);
+
+s3.display();
+
+s1.display2(02);
+
+s4.display();
+
+s1.display2(03);
+
+s5.display();
+
+s1.display2(10);
+
+
+
 }
--- recursion.cpp
+++ recursion.cpp
@@ -1,45 +1,60 @@
 
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
+class student {
+    public:
+string name,session,exam1,pren1,ct;
+int id,a;
 
+double gpa;
+void display(){
+  cout<<"name = "<<name<<endl<<"session = "<<session<<endl;
+  cout<<"id = "<<id<<" and "<<" cgpa id = "<<gpa<<endl;
+  cout<<endl;
 
-void insertionSort(int arr[], int n)
-{
-	int i, key, j;
-	for (i = 1; i < n; i++) {
-		key = arr[i];
-		j = i - 1;
-
-		
-		while (j >= 0 && arr[j] > key) {
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
-		arr[j + 1] = key;
-	}
 }
+  student(string name1,string session1,int id1,double b){
+  id=id1;
+  gpa=b;
+  name=name1;
+  session=session1;
+  }
+  void display2(int a1){
+
+    a=a1;
+    cout<<"CT result = "<<a<<endl;
+  }
+
+};
+
+int main(){
+student s1("Raj Adhikary","2019-2020",190109,3.45);
+student s2("Joydip das","2019-2020",190102,3.57);
+student s3("SHOPON ahmed","2019-2020",190107,3.75);
+student s4("Tasmia","2019-2020",190154,3.42);
+student s5("Rahul roy","2019-2020",190108,3.41);
 
 
-void printArray(int arr[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+s1.display();
+
+s1.display2(10);
+
+s2.display();
+
+s1.display2(01);
+
+s3.display();
+
+s1.display2(02);
+
+s4.display();
+
+s1.display2(03);
+
+s5.display();
+
+s1.display2(10);
+
+
+
 }
-
-
-int main()
-{
-	int arr[] = { 12, 11, 13, 5, 6 };
-	int N = sizeof(arr) / sizeof(arr[0]);
-
-	insertionSort(arr, N);
-	printArray(arr, N);
-int n, x;
-cin>>n>>x;
-cout<<n<<x;
-	return 0;
-	
-}
-
